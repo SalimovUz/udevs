a5a26374d99428c718c2877962f9a0bd93a83f85
SalimovUz <Salimofftolqin7@gmail.com>
2024-06-09 09:10:51 +0500

    Start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-0'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
